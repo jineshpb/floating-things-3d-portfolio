7149d699f422d2bd387c9ef10ee650a634008188
jineshpb <89720519+jineshpb@users.noreply.github.com>
2024-02-12 20:50:17 +0530

    Add TextBlock component to components list and update ContentList component

--- src/slices/ContentIndex/ContentList.tsx
+++ src/slices/ContentIndex/ContentList.tsx
@@ -1,5 +1,38 @@
+import { Content } from "@prismicio/client";
 import React from "react";
 
-export default function ContentList() {
-  return <div></div>;
+type ContentListProps = {
+  items: Content.BlogPostDocument[] | Content.ProjectDocument[];
+  contentType: Content.ContentIndexSlice["primary"]["content_type"];
+  fallbackItemImage: Content.ContentIndexSlice["primary"]["fallback_item_image"];
+  viewMoreText: Content.ContentIndexSlice["primary"]["view_more_text"];
+};
+
+export default function ContentList({
+  items,
+  contentType,
+  fallbackItemImage,
+  viewMoreText = "Read More",
+}: ContentListProps) {
+  return (
+    <div>
+      <ul>
+        {items.map((item, index) => (
+          <li key={index}>
+            <a href="">
+              <div>
+                <span>{item.data.title}</span>
+                <div>
+                  {item.tags.map((tag, index) => (
+                    <span key={index}>{tag}</span>
+                  ))}
+                </div>
+              </div>
+              <span>{viewMoreText}</span>
+            </a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
